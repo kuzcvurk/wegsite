f377d93ffbabb7f00bf921619d392460ed4d8580
Michelle McBeth <kuzcvurk@hotmail.com>
2017-06-24 10:30:48 -0400

    june 23rd
    New blog post & scout on home page.

--- Welcome_files/Welcome.js
+++ Welcome_files/Welcome.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20170319
+// Created by iWeb 3.0.4 local-build-20170624
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,-1,1,108),url:'Welcome_files/stroke.png'},{rect:new IWRect(-2,-2,1,1),url:'Welcome_files/stroke_1.png'},{rect:new IWRect(-1,-2,142,1),url:'Welcome_files/stroke_2.png'},{rect:new IWRect(141,-2,1,1),url:'Welcome_files/stroke_3.png'},{rect:new IWRect(141,-1,1,108),url:'Welcome_files/stroke_4.png'},{rect:new IWRect(141,107,1,1),url:'Welcome_files/stroke_5.png'},{rect:new IWRect(-1,107,142,1),url:'Welcome_files/stroke_6.png'},{rect:new IWRect(-2,107,1,1),url:'Welcome_files/stroke_7.png'}],new IWSize(140,106))});registry.applyEffects();}
